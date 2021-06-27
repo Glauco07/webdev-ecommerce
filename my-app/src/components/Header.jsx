56e5ce0c719eb14f0e474fcf001545570be777e9
Glauco <glaucohborges@gmail.com>
2021-06-26 22:14:58 -0300

    Fixed alimentos min height

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -4,9 +4,6 @@ import {loginUser} from "../store/reducers/userReducer"
 import { useDispatch, useSelector } from "react-redux";
 
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
     Link
 } from "react-router-dom";
 
